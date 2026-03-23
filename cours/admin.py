03797259f650c3ecbecb0e6ae58e8c0f8eda42ab
BERE Jean Clovis <jeanclovisbere@gmail.com>
2026-03-23 21:37:51 +0000

    Ajout du modèle Cours et configuration admin

--- cours/admin.py
+++ cours/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Cours
 
-# Register your models here.
+admin.site.register(Cours)
--- cours/models.py
+++ cours/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Cours(models.Model):
+    titre = models.CharField(max_length=200)
+    enseignant = models.CharField(max_length=100)
+    date_publication = models.DateField()
+
+    def __str__(self):
+        return self.titre
